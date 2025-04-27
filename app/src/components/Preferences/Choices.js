217fc45e9f5323f9f8e50975c2e27e1e6b57fde1
Ellie Xing <elliexin@usc.edu>
2025-04-26 18:14:06 -0700

    only 3 habits/concerns

--- app/src/components/Preferences/Choices.js
+++ app/src/components/Preferences/Choices.js
@@ -4,8 +4,10 @@ import "./Choices.css";
 function Choices({ filterName, updateData, formData, type }) {
     const isSelected = formData && formData[type] && formData[type].includes(filterName);
     const [isClicked, setIsClicked] = useState(isSelected);
+    const isDisabled = formData[type]?.length >= 3 && !isSelected;
 
     const handleClick = () => {
+        if (isDisabled) return;
         const updatedData = {
             ...formData,
             [type]: isSelected
@@ -19,7 +21,9 @@ function Choices({ filterName, updateData, formData, type }) {
     return (
         <div
             onClick={handleClick}
-            className={`filter-choice ${isClicked ? "when-clicked" : "when-not-clicked"}`}
+            // className={`filter-choice ${isClicked ? "when-clicked" : "when-not-clicked"}`}
+            className={`filter-choice ${isClicked ? "when-clicked" : "when-not-clicked"} ${isDisabled ? "disabled" : ""}`}
+            style={{ cursor: isDisabled ? "not-allowed" : "pointer" }}
         >
             <h1 className="filter-text">{filterName}</h1>
         </div>
